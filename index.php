2bbac9fd506c4070276cd1621167c068f22d49e0
Fahmi Syahrul yahya <yahya.fahmi01@outlook.com>
2019-10-19 22:54:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 echo "<h2>Selamat berlatih menjadi kolaborator</h2>";
 echo "<h3>Isi nama NIM anda di bawah baris code ini</h3>";
-echo "<h3>Aknis Sapriani (1954100025)</h3>";
+echo "<h3>Aknis Sapriani (1954100250)</h3>";
 
 ?>
