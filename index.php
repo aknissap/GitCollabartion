8d12b07528d7c2b0a83b553244f9bd0ef8ba541e
aknissap <aknissapriani4@gmail.com>
2019-10-19 19:48:43 +0700

    second commit

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 echo "<h2>Selamat berlatih menjadi kolaborator</h2>";
 echo "<h3>Isi nama NIM anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Satria Utomo W.P. (1555610024)</h3>";
+echo "<h3>Aknis Sapriani (195410025)</h3>";
 
 ?>
